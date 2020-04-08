fd83408b3f62cff6a501c333b691861c981c4d7d
Steven Westbrook <stevenwestbrook@Stevens-MacBook-Pro.local>
2020-04-08 15:21:07 -0400

    Changes to packaging process

--- CI/before-script-osx.sh
+++ CI/before-script-osx.sh
@@ -3,14 +3,16 @@ export PATH=/usr/local/opt/ccache/libexec:$PATH
 mkdir build
 cd build
 cmake \
+-DENABLE_SCRIPTING=OFF \
 -DDepsPath=/tmp/obsdeps \
--DCMAKE_BUILD_TYPE=RELEASE \
+-DCMAKE_BUILD_TYPE=Release \
 -DCMAKE_INSTALL_PREFIX=/opt/ebs \
 -DVLCPath=$PWD/../../vlc-master \
 -DQTDIR=/usr/local/Cellar/qt/5.10.1 \
 -DCMAKE_OSX_DEPLOYMENT_TARGET=10.10 \
 -DOPENSSL_ROOT_DIR=/usr/local/opt/openssl@1.1 \
--Dlibwebrtc_DIR=/tmp/libWebRTC-73.0-x64-Rel-COMMUNITY-BETA/cmake \
--DOBS_VERSION_OVERRIDE=23.2.0 \
+-Dlibwebrtc_DIR=../libwebrtc/cmake \
+-DBUILD_BROWSER=false \
+-DOBS_WEBRTC_VENDOR_NAME=Evercast \
+-DOBS_VERSION_OVERRIDE=2.5.0 \
 ..
-
