0ebc8a10b50fcdb71cadbc18b2f868c4eaded2ae
Steven Westbrook <steven.westbrook@clevertech.biz>
2020-04-09 10:12:42 -0400

    Got build and package working on Catalina

--- CI/before-script-osx.sh
+++ CI/before-script-osx.sh
@@ -4,14 +4,14 @@ mkdir build
 cd build
 cmake \
 -DENABLE_SCRIPTING=OFF \
--DDepsPath=/tmp/obsdeps \
+-DDepsPath=~/ct/obsdeps \
 -DCMAKE_BUILD_TYPE=Release \
 -DCMAKE_INSTALL_PREFIX=/opt/ebs \
 -DVLCPath=$PWD/../../vlc-master \
 -DQTDIR=/usr/local/Cellar/qt/5.10.1 \
 -DCMAKE_OSX_DEPLOYMENT_TARGET=10.10 \
 -DOPENSSL_ROOT_DIR=/usr/local/opt/openssl@1.1 \
--Dlibwebrtc_DIR=../libwebrtc/cmake \
+-Dlibwebrtc_DIR=~/ct/libwebrtc/cmake \
 -DBUILD_BROWSER=false \
 -DOBS_WEBRTC_VENDOR_NAME=Evercast \
 -DOBS_VERSION_OVERRIDE=2.5.0 \
