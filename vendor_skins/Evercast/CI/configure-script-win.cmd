642356429ddfd9700592368b459340d322172c00
SteveWestbrook <StevenAWestbrook@gmail.com>
2020-04-27 09:42:21 -0400

    Modified scripts for windows build

--- vendor_skins/Evercast/CI/configure-script-win.cmd
+++ vendor_skins/Evercast/CI/configure-script-win.cmd
@@ -1,6 +1,6 @@
 rem ---------------------------------------------------------------------
 cmake ^
-  -G "Visual Studio 15 2017 Win64" ^
+  -G "NMake Makefiles" ^
   -DENABLE_SCRIPTING=OFF ^
   -DCOPIED_DEPENDENCIES=false -DCOPY_DEPENDENCIES=true ^
   -DBUILD_CAPTIONS=false ^
@@ -12,4 +12,7 @@ cmake ^
   -DCMAKE_BUILD_TYPE=%build_config% ^
   -DOBS_WEBRTC_VENDOR_NAME=Evercast ^
   -DOBS_VERSION_OVERRIDE=%EBS_VERSION% ^
+  -DFFMPEG_AVCODEC_INCLUDE_DIRS=%ffmpegPath%\include ^
+  -DFFMPEG_AVCODEC_LIBRARIES=%ffmpegPath%\lib ^
+  -DDepsPath64=D:\deps\dependencies2017\win64 ^
   ..
