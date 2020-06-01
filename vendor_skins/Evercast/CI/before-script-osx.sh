fe63b5bbf01c5309d752c24117125f9216b76a47
Steven Westbrook <steven.westbrook@clevertech.biz>
2020-06-01 14:12:33 -0400

    Updated webrtc path

--- vendor_skins/Evercast/CI/before-script-osx.sh
+++ vendor_skins/Evercast/CI/before-script-osx.sh
@@ -11,7 +11,7 @@ cmake \
 -DQTDIR=/usr/local/Cellar/qt/5.14.1 \
 -DCMAKE_OSX_DEPLOYMENT_TARGET=10.12 \
 -DOPENSSL_ROOT_DIR=/usr/local/opt/openssl@1.1 \
--Dlibwebrtc_DIR=~/libwebrtc/cmake \
+-Dlibwebrtc_DIR=~/libwebrtc-79/cmake \
 -DBUILD_BROWSER=false \
 -DOBS_WEBRTC_VENDOR_NAME=Evercast \
 -DOBS_VERSION_OVERRIDE=$EBS_VERSION \
