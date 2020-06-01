1b96382b0b4e16e2d5235089a88d26a4798028fa
Steven Westbrook <steven.westbrook@clevertech.biz>
2020-06-01 14:18:45 -0400

    Disabled window capture in EBS

--- plugins/mac-capture/plugin-main.c
+++ plugins/mac-capture/plugin-main.c
@@ -10,13 +10,13 @@ MODULE_EXPORT const char *obs_module_description(void)
 extern struct obs_source_info coreaudio_input_capture_info;
 extern struct obs_source_info coreaudio_output_capture_info;
 extern struct obs_source_info display_capture_info;
-extern struct obs_source_info window_capture_info;
+// extern struct obs_source_info window_capture_info;
 
 bool obs_module_load(void)
 {
 	obs_register_source(&coreaudio_input_capture_info);
 	obs_register_source(&coreaudio_output_capture_info);
 	obs_register_source(&display_capture_info);
-	obs_register_source(&window_capture_info);
+	// obs_register_source(&window_capture_info);
 	return true;
 }
--- plugins/win-capture/plugin-main.c
+++ plugins/win-capture/plugin-main.c
@@ -12,7 +12,7 @@ MODULE_EXPORT const char *obs_module_description(void)
 
 extern struct obs_source_info duplicator_capture_info;
 extern struct obs_source_info monitor_capture_info;
-extern struct obs_source_info window_capture_info;
+// extern struct obs_source_info window_capture_info;
 extern struct obs_source_info game_capture_info;
 
 static HANDLE init_hooks_thread = NULL;
@@ -90,7 +90,7 @@ bool obs_module_load(void)
 
 	obs_leave_graphics();
 
-	obs_register_source(&window_capture_info);
+	// obs_register_source(&window_capture_info);
 
 	char *config_path = obs_module_config_path(NULL);
 
