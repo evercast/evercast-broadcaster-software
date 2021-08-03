a92b52e43b9948be95db90182b33068002f6e55c
lganzzzo <lganzzzo@gmail.com>
2021-08-03 17:28:13 +0300

    pack all dependencies recursively

--- CI/before-deploy-osx.sh
+++ CI/before-deploy-osx.sh
@@ -26,48 +26,48 @@ fi
 ../CI/install/osx/packageApp.sh
 
 # fix obs outputs
-hr "Fixing OBS outputs"
-cp /usr/local/opt/mbedtls/lib/libmbedtls.12.dylib ./EBS.app/Contents/Frameworks/
-cp /usr/local/opt/mbedtls/lib/libmbedcrypto.3.dylib ./EBS.app/Contents/Frameworks/
-cp /usr/local/opt/mbedtls/lib/libmbedx509.0.dylib ./EBS.app/Contents/Frameworks/
-
-cp /usr/local/lib/libcurl.4.dylib ./EBS.app/Contents/Frameworks/
-cp /usr/local/opt/nghttp2/lib/libnghttp2.14.dylib ./EBS.app/Contents/Frameworks/
-cp /usr/local/opt/libidn2/lib/libidn2.0.dylib ./EBS.app/Contents/Frameworks/
-cp /usr/local/opt/rtmpdump/lib/librtmp.1.dylib ./EBS.app/Contents/Frameworks/
-cp /usr/local/opt/zstd/lib/libzstd.1.dylib ./EBS.app/Contents/Frameworks/
-cp /usr/local/opt/brotli/lib/libbrotlidec.1.dylib ./EBS.app/Contents/Frameworks/
-cp /usr/local/opt/brotli/lib/libbrotlicommon.1.dylib ./EBS.app/Contents/Frameworks/
-cp /usr/local/opt/gettext/lib/libintl.8.dylib ./EBS.app/Contents/Frameworks/
-cp /usr/local/opt/libunistring/lib/libunistring.2.dylib ./EBS.app/Contents/Frameworks/
-
-install_name_tool -change /usr/local/opt/nghttp2/lib/libnghttp2.14.dylib @executable_path/../Frameworks/libnghttp2.14.dylib ./EBS.app/Contents/Frameworks/libcurl.4.dylib
-install_name_tool -change /usr/local/opt/libidn2/lib/libidn2.0.dylib @executable_path/../Frameworks/libidn2.0.dylib ./EBS.app/Contents/Frameworks/libcurl.4.dylib
-install_name_tool -change /usr/local/opt/rtmpdump/lib/librtmp.1.dylib @executable_path/../Frameworks/librtmp.1.dylib ./EBS.app/Contents/Frameworks/libcurl.4.dylib
-install_name_tool -change /usr/local/opt/openssl@1.1/lib/libssl.1.1.dylib @executable_path/../Frameworks/libssl.1.1.dylib ./EBS.app/Contents/Frameworks/libcurl.4.dylib
-install_name_tool -change /usr/local/opt/openssl@1.1/lib/libcrypto.1.1.dylib @executable_path/../Frameworks/libcrypto.1.1.dylib ./EBS.app/Contents/Frameworks/libcurl.4.dylib
-install_name_tool -change /usr/local/opt/zstd/lib/libzstd.1.dylib @executable_path/../Frameworks/libzstd.1.dylib ./EBS.app/Contents/Frameworks/libcurl.4.dylib
-install_name_tool -change /usr/local/opt/brotli/lib/libbrotlidec.1.dylib @executable_path/../Frameworks/libbrotlidec.1.dylib ./EBS.app/Contents/Frameworks/libcurl.4.dylib
-
-install_name_tool -change /usr/local/opt/gettext/lib/libintl.8.dylib @executable_path/../Frameworks/libintl.8.dylib ./EBS.app/Contents/Frameworks/libidn2.0.dylib
-install_name_tool -change /usr/local/opt/libunistring/lib/libunistring.2.dylib @executable_path/../Frameworks/libunistring.2.dylib ./EBS.app/Contents/Frameworks/libidn2.0.dylib
-
-install_name_tool -change /usr/local/opt/openssl@1.1/lib/libssl.1.1.dylib @executable_path/../Frameworks/libssl.1.1.dylib ./EBS.app/Contents/Frameworks/librtmp.1.dylib
-install_name_tool -change /usr/local/opt/openssl@1.1/lib/libcrypto.1.1.dylib @executable_path/../Frameworks/libcrypto.1.1.dylib ./EBS.app/Contents/Frameworks/librtmp.1.dylib
-
-install_name_tool -change /usr/local/opt/mbedtls/lib/libmbedtls.12.dylib @executable_path/../Frameworks/libmbedtls.12.dylib ./EBS.app/Contents/Plugins/obs-outputs.so
-install_name_tool -change /usr/local/opt/mbedtls/lib/libmbedcrypto.3.dylib @executable_path/../Frameworks/libmbedcrypto.3.dylib ./EBS.app/Contents/Plugins/obs-outputs.so
-install_name_tool -change /usr/local/opt/mbedtls/lib/libmbedx509.0.dylib @executable_path/../Frameworks/libmbedx509.0.dylib ./EBS.app/Contents/Plugins/obs-outputs.so
-install_name_tool -change /usr/local/opt/openssl@1.1/lib/libssl.1.1.dylib @executable_path/../Frameworks/libssl.1.1.dylib ./EBS.app/Contents/Plugins/obs-outputs.so
-install_name_tool -change /usr/local/opt/openssl@1.1/lib/libcrypto.1.1.dylib @executable_path/../Frameworks/libcrypto.1.1.dylib ./EBS.app/Contents/Plugins/obs-outputs.so
-install_name_tool -change /usr/local/opt/curl/lib/libcurl.4.dylib @executable_path/../Frameworks/libcurl.4.dylib ./EBS.app/Contents/Plugins/obs-outputs.so
-install_name_tool -change /usr/lib/libcurl.4.dylib @executable_path/../Frameworks/libcurl.4.dylib ./EBS.app/Contents/Plugins/obs-outputs.so
-install_name_tool -change @rpath/libobs.0.dylib @executable_path/../Frameworks/libobs.0.dylib ./EBS.app/Contents/Plugins/obs-outputs.so
-install_name_tool -change /usr/local/opt/qt5/lib/QtGui.framework/Versions/5/QtGui @executable_path/../Frameworks/QtGui.framework/Versions/5/QtGui ./EBS.app/Contents/Plugins/obs-outputs.so
-install_name_tool -change /usr/local/opt/qt5/lib/QtCore.framework/Versions/5/QtCore @executable_path/../Frameworks/QtCore.framework/Versions/5/QtCore ./EBS.app/Contents/Plugins/obs-outputs.so
-install_name_tool -change /usr/local/opt/qt5/lib/QtWidgets.framework/Versions/5/QtWidgets @executable_path/../Frameworks/QtWidgets.framework/Versions/5/QtWidgets ./EBS.app/Contents/Plugins/obs-outputs.so
-install_name_tool -change /usr/local/opt/qt5/lib/QtMacExtras.framework/Versions/5/QtMacExtras @executable_path/../Frameworks/QtMacExtras.framework/Versions/5/QtMacExtras ./EBS.app/Contents/Plugins/obs-outputs.so
-install_name_tool -change /usr/local/opt/qt5/lib/QtSvg.framework/Versions/5/QtSvg @executable_path/../Frameworks/QtSvg.framework/Versions/5/QtSvg ./EBS.app/Contents/Plugins/obs-outputs.so
+#hr "Fixing OBS outputs"
+#cp /usr/local/opt/mbedtls/lib/libmbedtls.12.dylib ./EBS.app/Contents/Frameworks/
+#cp /usr/local/opt/mbedtls/lib/libmbedcrypto.3.dylib ./EBS.app/Contents/Frameworks/
+#cp /usr/local/opt/mbedtls/lib/libmbedx509.0.dylib ./EBS.app/Contents/Frameworks/
+#
+#cp /usr/local/lib/libcurl.4.dylib ./EBS.app/Contents/Frameworks/
+#cp /usr/local/opt/nghttp2/lib/libnghttp2.14.dylib ./EBS.app/Contents/Frameworks/
+#cp /usr/local/opt/libidn2/lib/libidn2.0.dylib ./EBS.app/Contents/Frameworks/
+#cp /usr/local/opt/rtmpdump/lib/librtmp.1.dylib ./EBS.app/Contents/Frameworks/
+#cp /usr/local/opt/zstd/lib/libzstd.1.dylib ./EBS.app/Contents/Frameworks/
+#cp /usr/local/opt/brotli/lib/libbrotlidec.1.dylib ./EBS.app/Contents/Frameworks/
+#cp /usr/local/opt/brotli/lib/libbrotlicommon.1.dylib ./EBS.app/Contents/Frameworks/
+#cp /usr/local/opt/gettext/lib/libintl.8.dylib ./EBS.app/Contents/Frameworks/
+#cp /usr/local/opt/libunistring/lib/libunistring.2.dylib ./EBS.app/Contents/Frameworks/
+#
+#install_name_tool -change /usr/local/opt/nghttp2/lib/libnghttp2.14.dylib @executable_path/../Frameworks/libnghttp2.14.dylib ./EBS.app/Contents/Frameworks/libcurl.4.dylib
+#install_name_tool -change /usr/local/opt/libidn2/lib/libidn2.0.dylib @executable_path/../Frameworks/libidn2.0.dylib ./EBS.app/Contents/Frameworks/libcurl.4.dylib
+#install_name_tool -change /usr/local/opt/rtmpdump/lib/librtmp.1.dylib @executable_path/../Frameworks/librtmp.1.dylib ./EBS.app/Contents/Frameworks/libcurl.4.dylib
+#install_name_tool -change /usr/local/opt/openssl@1.1/lib/libssl.1.1.dylib @executable_path/../Frameworks/libssl.1.1.dylib ./EBS.app/Contents/Frameworks/libcurl.4.dylib
+#install_name_tool -change /usr/local/opt/openssl@1.1/lib/libcrypto.1.1.dylib @executable_path/../Frameworks/libcrypto.1.1.dylib ./EBS.app/Contents/Frameworks/libcurl.4.dylib
+#install_name_tool -change /usr/local/opt/zstd/lib/libzstd.1.dylib @executable_path/../Frameworks/libzstd.1.dylib ./EBS.app/Contents/Frameworks/libcurl.4.dylib
+#install_name_tool -change /usr/local/opt/brotli/lib/libbrotlidec.1.dylib @executable_path/../Frameworks/libbrotlidec.1.dylib ./EBS.app/Contents/Frameworks/libcurl.4.dylib
+#
+#install_name_tool -change /usr/local/opt/gettext/lib/libintl.8.dylib @executable_path/../Frameworks/libintl.8.dylib ./EBS.app/Contents/Frameworks/libidn2.0.dylib
+#install_name_tool -change /usr/local/opt/libunistring/lib/libunistring.2.dylib @executable_path/../Frameworks/libunistring.2.dylib ./EBS.app/Contents/Frameworks/libidn2.0.dylib
+#
+#install_name_tool -change /usr/local/opt/openssl@1.1/lib/libssl.1.1.dylib @executable_path/../Frameworks/libssl.1.1.dylib ./EBS.app/Contents/Frameworks/librtmp.1.dylib
+#install_name_tool -change /usr/local/opt/openssl@1.1/lib/libcrypto.1.1.dylib @executable_path/../Frameworks/libcrypto.1.1.dylib ./EBS.app/Contents/Frameworks/librtmp.1.dylib
+#
+#install_name_tool -change /usr/local/opt/mbedtls/lib/libmbedtls.12.dylib @executable_path/../Frameworks/libmbedtls.12.dylib ./EBS.app/Contents/Plugins/obs-outputs.so
+#install_name_tool -change /usr/local/opt/mbedtls/lib/libmbedcrypto.3.dylib @executable_path/../Frameworks/libmbedcrypto.3.dylib ./EBS.app/Contents/Plugins/obs-outputs.so
+#install_name_tool -change /usr/local/opt/mbedtls/lib/libmbedx509.0.dylib @executable_path/../Frameworks/libmbedx509.0.dylib ./EBS.app/Contents/Plugins/obs-outputs.so
+#install_name_tool -change /usr/local/opt/openssl@1.1/lib/libssl.1.1.dylib @executable_path/../Frameworks/libssl.1.1.dylib ./EBS.app/Contents/Plugins/obs-outputs.so
+#install_name_tool -change /usr/local/opt/openssl@1.1/lib/libcrypto.1.1.dylib @executable_path/../Frameworks/libcrypto.1.1.dylib ./EBS.app/Contents/Plugins/obs-outputs.so
+#install_name_tool -change /usr/local/opt/curl/lib/libcurl.4.dylib @executable_path/../Frameworks/libcurl.4.dylib ./EBS.app/Contents/Plugins/obs-outputs.so
+#install_name_tool -change /usr/lib/libcurl.4.dylib @executable_path/../Frameworks/libcurl.4.dylib ./EBS.app/Contents/Plugins/obs-outputs.so
+#install_name_tool -change @rpath/libobs.0.dylib @executable_path/../Frameworks/libobs.0.dylib ./EBS.app/Contents/Plugins/obs-outputs.so
+#install_name_tool -change /usr/local/opt/qt5/lib/QtGui.framework/Versions/5/QtGui @executable_path/../Frameworks/QtGui.framework/Versions/5/QtGui ./EBS.app/Contents/Plugins/obs-outputs.so
+#install_name_tool -change /usr/local/opt/qt5/lib/QtCore.framework/Versions/5/QtCore @executable_path/../Frameworks/QtCore.framework/Versions/5/QtCore ./EBS.app/Contents/Plugins/obs-outputs.so
+#install_name_tool -change /usr/local/opt/qt5/lib/QtWidgets.framework/Versions/5/QtWidgets @executable_path/../Frameworks/QtWidgets.framework/Versions/5/QtWidgets ./EBS.app/Contents/Plugins/obs-outputs.so
+#install_name_tool -change /usr/local/opt/qt5/lib/QtMacExtras.framework/Versions/5/QtMacExtras @executable_path/../Frameworks/QtMacExtras.framework/Versions/5/QtMacExtras ./EBS.app/Contents/Plugins/obs-outputs.so
+#install_name_tool -change /usr/local/opt/qt5/lib/QtSvg.framework/Versions/5/QtSvg @executable_path/../Frameworks/QtSvg.framework/Versions/5/QtSvg ./EBS.app/Contents/Plugins/obs-outputs.so
 
 cp ../CI/install/osx/OBSPublicDSAKey.pem EBS.app/Contents/Resources
 
