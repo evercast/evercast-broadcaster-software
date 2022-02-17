910a14293f3f65d96f054f236435bfc4d0408042
lganzzzo <lganzzzo@gmail.com>
2022-02-17 16:05:52 +0300

    get rid of gdrive

--- CI/util/gd_wait_cookie.sh
+++
@@ -1,9 +0,0 @@
-while true ; do
-    curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}" > /dev/null
-    cookie=`cat cookie | grep google`
-    if [ "$cookie" != "" ]; then
-        break
-    fi
-    echo "waiting cookie..."
-    sleep 1
-done
