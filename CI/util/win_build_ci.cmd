db9a76f5406e8a3951ab9ca0407e9c1b0f0a3460
lganzzzo <lganzzzo@gmail.com>
2022-03-22 17:42:02 +0200

    fix script

--- CI/util/win_build_ci.cmd
+++ CI/util/win_build_ci.cmd
@@ -1,11 +1,13 @@
 @ECHO OFF
+
+SET PATH=%PATH%;"C:\Program Files\CMake\bin"
+SET PATH=%PATH%;"C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Tools\MSVC\14.16.27023\bin\Hostx64\x64"
+
+ECHO %PATH%
+
 "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
 
-set PATH=%PATH%;"C:\Program Files\CMake\bin"
-set PATH=%PATH%;"C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Tools\MSVC\14.16.27023\bin\Hostx64\x64"
-
-echo %PATH%
+CD ..\..\build
 
 ..\CI\configure-script-win.cmd
 nmake
-
