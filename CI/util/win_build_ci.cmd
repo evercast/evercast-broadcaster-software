a9244d0d859044a073338acf71964d42d3d87ea2
Steven Westbrook <stevenawestbrook@gmail.com>
2022-10-28 09:00:51 -0400

    Echo on

--- CI/util/win_build_ci.cmd
+++ CI/util/win_build_ci.cmd
@@ -1,4 +1,4 @@
-@ECHO OFF
+rem @ECHO OFF
 
 SET PATH=%PATH%;"C:\Program Files\CMake\bin"
 SET PATH=%PATH%;"C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Tools\MSVC\14.16.27023\bin\Hostx64\x64"
