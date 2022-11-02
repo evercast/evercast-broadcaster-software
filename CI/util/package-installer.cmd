b129e14ce2152f3f96c5bfd24dd0b9f52e2763b9
Steven Westbrook <stevenawestbrook@gmail.com>
2022-11-01 21:50:10 -0400

    Added temporary cache to speed up testing process, some debugging logic

--- CI/util/package-installer.cmd
+++ CI/util/package-installer.cmd
@@ -1,5 +1,8 @@
-cd ..\..\build
+CD ..\..\build
 "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\IDE\CommonExtensions\Microsoft\CMake\CMake\bin\cpack.exe" -G WIX
+
+PRINT C:/Users/circleci/project/build/_CPack_Packages/win64/WIX/PreinstallOutput.log
+
 "%WIX%\bin\candle.exe" "..\CI\install\win\Install EBS.wxs" -ext WixBalExtension -ext WixUtilExtension
 "%WIX%\bin\light.exe" "Install EBS.wixobj" -ext WixBalExtension -ext WixUtilExtension
-move "Install EBS.exe" "Install EBS %EBS_VERSION%.exe"
+MOVE "Install EBS.exe" "Install EBS %EBS_VERSION%.exe"
