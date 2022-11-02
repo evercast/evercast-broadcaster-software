9ea474fd8ffef0253fb80e5ac3b57e9cef0ba5fe
Steven Westbrook <stevenawestbrook@gmail.com>
2022-11-01 22:20:01 -0400

    type not print

--- CI/util/package-installer.cmd
+++ CI/util/package-installer.cmd
@@ -1,7 +1,7 @@
 CD ..\..\build
 "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\IDE\CommonExtensions\Microsoft\CMake\CMake\bin\cpack.exe" -G WIX
 
-PRINT C:\Users\circleci\project\build\_CPack_Packages\win64\WIX\PreinstallOutput.log
+TYPE C:\Users\circleci\project\build\_CPack_Packages\win64\WIX\PreinstallOutput.log
 
 "%WIX%\bin\candle.exe" "..\CI\install\win\Install EBS.wxs" -ext WixBalExtension -ext WixUtilExtension
 "%WIX%\bin\light.exe" "Install EBS.wixobj" -ext WixBalExtension -ext WixUtilExtension
