e46e17d34e60ab73e2339c0a93890fc42459122f
Steven Westbrook <stevenawestbrook@gmail.com>
2022-11-02 20:42:08 -0400

    More logging

--- CI/util/package-installer.cmd
+++ CI/util/package-installer.cmd
@@ -1,11 +1,15 @@
-SET WIX=%USERPROFILE%/project/CI/tools/wix
+SET WIX=%USERPROFILE%\project\CI\tools\wix
 ECHO %WIX%
 SET PATH=%PATH%;%WIX%
-CD ..\..\build
 DIR
 "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\IDE\CommonExtensions\Microsoft\CMake\CMake\bin\cpack.exe" -G WIX --verbose
 
+ECHO "****************************** Preinstall log ******************************"
 TYPE .\_CPack_Packages\win64\WIX\PreinstallOutput.log
+ECHO "****************************** Output log ******************************"
+TYPE .\CMakeFiles\CMakeOutput.log
+ECHO "****************************** Error log ******************************"
+TYPE .\CMakeFiles\CMakeError.log
 
 "%WIX%\candle.exe" "..\CI\install\win\Install EBS.wxs" -ext WixBalExtension -ext WixUtilExtension
 "%WIX%\light.exe" "Install EBS.wixobj" -ext WixBalExtension -ext WixUtilExtension
