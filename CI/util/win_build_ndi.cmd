fbce7a0e357ac79ab0d6242c77aa435be8e3e1d2
Steven Westbrook <stevenawestbrook@gmail.com>
2022-11-07 08:42:15 -0500

    Additional path may be required for NDI build

--- CI/util/win_build_ndi.cmd
+++ CI/util/win_build_ndi.cmd
@@ -1,7 +1,7 @@
 SET PATH=%PATH%;C:\Program Files\CMake\bin
 SET PATH=%PATH%;C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Tools\MSVC\14.16.27023\bin\Hostx64\x64
 
-SET EBS_DIR=..\..\..
+SET EBS_DIR=..\..\..\evercast-broadcaster-software
 SET QTDIR64=C:\Users\circleci\project\deps-install\Qt\5.15.2\msvc2019_64
 
 CALL "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
@@ -16,6 +16,7 @@ CD build
 cmake ^
   -G "NMake Makefiles" ^
   -DLIBOBS_INCLUDE_DIR=%EBS_DIR%\libobs ^
+  -DLIBOBS_DIR=%EBS_DIR%\libobs ^
   -DLIBOBS_LIB=%EBS_DIR%\build\libobs\obs.lib ^
   -DOBS_FRONTEND_LIB=%EBS_DIR%\build\UI\obs-frontend-api\obs-frontend-api.lib ^
   -DQt5Core_DIR=%QTDIR64%/lib/cmake/Qt5Core ^
