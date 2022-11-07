9e4004bc37b166d04e9904b81a08cd2fd401314c
Steven Westbrook <stevenawestbrook@gmail.com>
2022-11-07 07:14:57 -0500

    Another path fix

--- CI/util/win_build_ndi.cmd
+++ CI/util/win_build_ndi.cmd
@@ -1,7 +1,7 @@
 SET PATH=%PATH%;C:\Program Files\CMake\bin
 SET PATH=%PATH%;C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Tools\MSVC\14.16.27023\bin\Hostx64\x64
 
-SET EBS_DIR=..\..
+SET EBS_DIR=..\..\..
 SET QTDIR64=C:\Users\circleci\project\deps-install\Qt\5.15.2\msvc2019_64
 
 CALL "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
