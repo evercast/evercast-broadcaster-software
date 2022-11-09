7c89f54766a7fe3a32a11e43d3700296ac05c985
Steven Westbrook <stevenawestbrook@gmail.com>
2022-11-09 07:57:48 -0500

    Fixed up variables based on minimum input

--- CI/util/win_build_ndi.cmd
+++ CI/util/win_build_ndi.cmd
@@ -1,8 +1,8 @@
 SET PATH=%PATH%;C:\Program Files\CMake\bin
 SET PATH=%PATH%;C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Tools\MSVC\14.16.27023\bin\Hostx64\x64
 
-SET EBS_DIR=..\..\..
-SET QTDIR64=C:\Users\circleci\project\deps-install\Qt\5.15.2\msvc2019_64
+SET EBS_DIR=../../..
+SET QTDIR64=C:/Users/circleci/project/deps-install/Qt/5.15.2/msvc2019_64
 
 CALL "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
 
@@ -13,17 +13,19 @@ CD ..\..\deps-install\obs-ndi
 MKDIR build
 CD build
 
+rem -DCMAKE_MODULE_PATH=C:/Users/circleci/project/deps-install/obs-ndi/external ^
 cmake ^
   -G "NMake Makefiles" ^
   -DCMAKE_BUILD_TYPE=Release ^
-  -DCMAKE_MODULE_PATH=C:/Users/circleci/project/deps-install/obs-ndi/external ^
-  -DCMAKE_PREFIX_PATH=C:/Users/circleci/project/build/deps/w32-pthreads ^
-  -DLIBOBS_INCLUDE_DIR=%EBS_DIR%\libobs ^
-  -DLIBOBS_LIB=%EBS_DIR%\build\libobs\obs.lib ^
-  -DOBS_FRONTEND_LIB=%EBS_DIR%\build\UI\obs-frontend-api\obs-frontend-api.lib ^
+  -DLIBOBS_INCLUDE_DIR=%EBS_DIR%/libobs ^
+  -DLIBOBS_LIB=%EBS_DIR%/build/libobs/obs.lib ^
+  -DLIBOBS_DIR=%EBS_DIR%/build/libobs ^
+  -DOBS_FRONTEND_LIB=%EBS_DIR%/build/UI/obs-frontend-api/obs-frontend-api.lib ^
   -DQt5Core_DIR=%QTDIR64%/lib/cmake/Qt5Core ^
   -DQt5Widgets_DIR=%QTDIR64%/lib/cmake/Qt5Widgets ^
   -DQTDIR=%QTDIR64% ^
+  -DW32_PTHREADS_LIB=C:/Users/circleci/project/build/deps/w32-pthreads ^
+  -Dw32-pthreads_DIR=C:/Users/circleci/project/build/deps/w32-pthreads ^
   ..
 
 nmake
