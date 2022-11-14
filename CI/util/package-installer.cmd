e05a48df5b06ad6a59f9de81ad01811eb4a2d194
Steven Westbrook <stevenawestbrook@gmail.com>
2022-11-14 08:52:10 -0500

    Manually entered wix path

--- CI/util/package-installer.cmd
+++ CI/util/package-installer.cmd
@@ -4,11 +4,15 @@ SET PATH=%PATH%;%WIX%
 SET PATH=%PATH%;C:\Program Files\CMake\bin
 SET PATH=%PATH%;C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Tools\MSVC\14.16.27023\bin\Hostx64\x64
 
+DEL .\_CPack_Packages\win64\WIX\PreinstallOutput.log
+DEL .\CMakeFiles\CMakeOutput.log
+DEL .\CMakeFiles\CMakeError.log
+
 ECHO %PATH%
 
 CALL "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
 
-"C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\IDE\CommonExtensions\Microsoft\CMake\CMake\bin\cpack.exe" -G WIX --verbose --debug
+"C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\IDE\CommonExtensions\Microsoft\CMake\CMake\bin\cpack.exe" -G WIX --verbose --debug -DCPACK_WIX_ROOT=C:/users/circleci/project/CI/tools
 
 ECHO "****************************** Preinstall log ******************************"
 TYPE .\_CPack_Packages\win64\WIX\PreinstallOutput.log
